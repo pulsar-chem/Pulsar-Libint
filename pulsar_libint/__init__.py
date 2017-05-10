3dc7b83004a0be5604f153e8f89923a5a0d38c0d
ryanmrichard <ryanmrichard1@gmail.com>
2017-05-10 18:07:18 -0500

    Missed a file

--- pulsar_libint/__init__.py
+++ pulsar_libint/__init__.py
@@ -6,3 +6,6 @@ def initialize(mm):
     mm.load_module("pulsar_libint","Kinetic","LIBINT_T")
     mm.load_module("pulsar_libint","Overlap","LIBINT_S")
     mm.load_module("pulsar_libint","ERI","LIBINT_ERI")
+    mm.load_module("pulsar_libint","DF3C2E","LIBINT_3C2E")
+    mm.load_module("pulsar_libint","Metric","LIBINT_Metric")
+
